41c0d0afe47dfc60f019b49d3f25a4a860af051d
lewishe <lewishe@outlook.com>
2024-01-16 09:05:34 +0800

    Added SIM7600 GNSS Power Control

--- src/TinyGsmClientSIM7600.h
+++ src/TinyGsmClientSIM7600.h
@@ -460,12 +460,32 @@ class TinyGsmSim7600 : public TinyGsmModem<TinyGsmSim7600>,
  protected:
   // enable GPS
   bool enableGPSImpl(int8_t power_en_pin ,uint8_t enable_level) {
+    if(power_en_pin == GSM_MODEM_AUX_POWER){
+      sendAT("+CVAUXV=2800");
+      waitResponse();
+      sendAT("+CVAUXS=1");
+      waitResponse();
+    }else if(power_en_pin != -1){
+      sendAT("+CGDRT=",power_en_pin,",1");
+      waitResponse();
+      sendAT("+CGSETV=",power_en_pin,",",enable_level);
+      waitResponse();
+    } 
     sendAT(GF("+CGPS=1"));
     if (waitResponse() != 1) { return false; }
     return true;
   }
 
   bool disableGPSImpl(int8_t power_en_pin ,uint8_t disbale_level) {
+    if(power_en_pin == GSM_MODEM_AUX_POWER){
+      sendAT("+CVAUXS=0");
+      waitResponse();
+    }else if(power_en_pin != -1){
+      sendAT("+CGSETV=",power_en_pin,",",disbale_level);
+      waitResponse();
+      sendAT("+CGDRT=",power_en_pin,",0");
+      waitResponse();
+    } 
     sendAT(GF("+CGPS=0"));
     if (waitResponse() != 1) { return false; }
     return waitResponse(30000UL,"+CGPS: 0") == 1;
